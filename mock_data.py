ffb670643a59248d7284b8dd5ca17962366915b5
Jessie1331 <141692878+Jessie1331@users.noreply.github.com>
2023-11-06 17:27:40 -0800

    Add files via upload

---
+++ mock_data.py
@@ -0,0 +1,42 @@
+Catalog =  [
+{
+"title": "Flash",
+"catagory":"Flashers",
+'Price': 970,
+"image": "flash.webp",
+"id":'1'
+},
+
+
+{
+"title": "Elegent",
+"catagory":"Heels",
+'Price': 590,
+"image": "elegent.webp",
+'id':'2'
+},
+    
+{
+"title": "How High",
+"catagory":"Hightops", 
+'Price': 580,
+"image": "mens hightops.webp",
+'id':'3'
+}
+] 
+
+coupon_codes =[
+    
+{'code': 'mememi'},
+{'discount': '40'},
+
+{'code': 'mememi'},
+{'discount': '10'},
+
+{'code': 'misshim'},
+{'discount': '60'},
+
+{'code': 'wish'},
+{'discount': '26'}
+
+]
